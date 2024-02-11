d5fd30e6c519a5e91da4b8bbc56efff36d97219c
leonardomorita <leonardomorita2@gmail.com>
2024-02-11 18:38:23 -0300

    🚀 Aula 5

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 function tocaSom(tecla) {
+    if (!document.querySelector(`#som_${tecla}`)) {
+        return false;
+    }
+    
     document.querySelector(`#som_${tecla}`).play(); // Reproduzir o som
 }
 
 const teclas = document.querySelectorAll('.tecla');
 teclas.forEach((tecla, contador) => {
     let classeTecla = tecla.classList[1];
+
     tecla.onclick = () => {
         tocaSom(classeTecla);
     };
-    console.log(contador);
+
+    tecla.onkeydown = (evento) => {
+        if (evento.code === 'Enter' || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    };
+
+    tecla.onkeyup = () => {
+        tecla.classList.remove('ativa');
+    }
+
+    // console.log(contador);
 });
